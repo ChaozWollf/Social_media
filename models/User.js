6ecd8cb4bfea1a570c3c30613f642d6afd953497
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-08-23 18:27:08 -0500

    worked on the user schema

--- models/User.js
+++ models/User.js
@@ -1,19 +1,36 @@
 const { Schema, model } = require('mongoose');
 const userSchema = new mongoose.Schema(
-{
-    username: {type:String, required:true},
-    friendslist:{ type:Array, required:true},
+    {
+        username: String, required: true,
+        friendslist: Array, required: true,
+    
+    thoughts: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'thoughts',
+    },
+],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
 
-//         const grocerySchema = new mongoose.Schema({
-//             // Add individual properties and their types
-//             // Setting required to true will disallow null values
-//             item: { type: String, required: true },
-//             stockCount: Number,
-//             price: Number,
-//             inStock: Boolean,
-//             // Use built in date method to get current date
-//             lastAccessed: { type: Date, default: Date.now },
-//         });
-// }
 
-})
+
+userSchema
+    .virtual('users')
+    .get(function () {
+        return `${this.username}`;
+    })
+    .set(function (v) {
+        const username = v.split(' ')[0];
+        this.set({ username });
+    });
+
+const User = model('user', userSchema);
+
+module.exports = User;
--- models/index.js
+++ models/index.js
@@ -2,4 +2,5 @@ const user = require('./User');
 const thoughts = require('./Thought');
 const reactions = require('./Reaction');
 
+
 module.exports = { user, thoughts, reactions }

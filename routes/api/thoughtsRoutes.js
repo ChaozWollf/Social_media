5b9d9fdea7c503f0ca9f0ca0c294baf97a02004a
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-08-25 13:44:15 -0500

    working on thoughts routes

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -14,7 +14,6 @@ router.route('/').get(getThoughts).post(createThought);
 router
   .route('/:thoughtId')
   .get(getSingleThought)
-  .put(updateThought)
   .delete(deleteThought);
 
 module.exports = router;

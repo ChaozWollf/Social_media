82250e8139929879a3be3cb5a98c057590b4b5d1
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-08-25 15:38:11 -0500

    working on getting things to work

--- models/Thought.js
+++ models/Thought.js
@@ -1,12 +1,11 @@
 const { Schema, model } = require('mongoose');
 
-
 const thoughtSchema = new Schema(
     {
         text: String,
         username: String,
         comments: [{ type: Schema.Types.ObjectId, ref: 'thoughts' }],
-        reactions: [reactionsSchema]
+        // reactions: [reactionSchema]
     },
 
     {
